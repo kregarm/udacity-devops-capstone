f6f76eeca21a7c8d434416251a67883d598bbe79
Martin Kregar <martinkregar@gmail.com>
2020-05-31 13:36:20 +0200

    added eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let express = require('express');
+let express = require("express");
 
 let app = express();
 
-app.get('/', (req, res) => {
-    console.log('Incoming request')
-    res.send('Hello world from simple Node server')
-})
+app.get("/", (req, res) => {
+    console.log("Incoming request");
+    res.send("Hello world from simple Node server");
+});
 
 app.listen(8080, () => {
-    console.log('running')
-})
+    console.log("running");
+});
